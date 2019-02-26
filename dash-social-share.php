54c8ec0e1458cb2519558ee979a5b794461d15c5
Marcel Badua <marcel.badua@ballistix.com>
2019-02-26 02:31:28 +0000

    1.1.1

--- dash-social-share.php
+++ dash-social-share.php
@@ -4,7 +4,7 @@
 Plugin Name: Dash Social Share
 Plugin URI: 
 Description: Add social share links on every page.
-Version: 1.0.1
+Version: 1.1.1
 Author: Marcel Badua
 Author URI: http://marcelbadua.com/
 License: GPL2
@@ -28,6 +28,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
 require_once 'libs/Mobile_Detect.php';
+require_once 'libs/updater.php';
 
 if (!class_exists('DASH_SOCIAL_SHARE')) {
     class DASH_SOCIAL_SHARE
@@ -36,7 +37,23 @@ if (!class_exists('DASH_SOCIAL_SHARE')) {
          * Construct the plugin object
          */
         public function __construct() {
-
+            
+            if (is_admin()) { // note the use of is_admin() to double check that this is happening in the admin
+        		$config = array(
+        			'slug' => plugin_basename(__FILE__), // this is the slug of your plugin
+        			'proper_folder_name' => 'dash-social-share', // this is the name of the folder your plugin lives in
+        			'api_url' => 'https://api.github.com/repos/marcelbadua/dash-social-share', // the GitHub API url of your GitHub repo
+        			'raw_url' => 'https://raw.github.com/marcelbadua/dash-social-share/master', // the GitHub raw url of your GitHub repo
+        			'github_url' => 'https://github.com/marcelbadua/dash-social-share', // the GitHub url of your GitHub repo
+        			'zip_url' => 'https://github.com/marcelbadua/dash-social-share/zipball/master', // the zip url of the GitHub repo
+        			'sslverify' => true, // whether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
+        			'requires' => '3.0', // which version of WordPress does your plugin require?
+        			'tested' => '3.3', // which version of WordPress is your plugin tested up to?
+        			'readme' => 'README.md', // which file to use as the readme for the version number
+        			'access_token' => '', // Access private repositories by authorizing under Appearance > GitHub Updates when this example plugin is installed
+        		);
+        		new WP_GitHub_Updater($config);
+        	}
             // Initialize Settings
             require_once(sprintf("%s/settings.php", dirname(__FILE__)));
             $DASH_SOCIAL_SHARE_SETTINGS = new DASH_SOCIAL_SHARE_SETTINGS();
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Dash Social Share
+
+~Current Version:1.1.1~
+
+Add social share links on every page
